f1e391c290f3c95af471dc7d6414dd3681c77a5e
Kevin Ndung'u <kevgathuku@gmail.com>
2016-10-25 10:19:59 +0300

    Refactor validate-form method to use domina

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,16 +1,13 @@
-(ns modern-cljs.login)
+(ns modern-cljs.login
+  (:require [domina.core :refer [by-id value set-value!]]))
 
-(defn validate-form
-  []
-  (let [email (.getElementById js/document "email")
-        password (.getElementById js/document "password")]
-
-    (if (and (> (count (.-value email)) 0)
-             (> (count (.-value password)) 0))
-      true
-      ; returns the value of the last experssion i.e. false
-      (do (js/alert "Please, complete the form!")
-          false))))
+(defn validate-form []
+  (if (and (> (count (value (by-id "email"))) 0)
+           (> (count (value (by-id "password"))) 0))
+    true
+    ; returns the value of the last experssion i.e. false
+    (do (js/alert "Please, complete the form!")
+        false)))
 
 ;; define the function to attach validate-form to onsubmit event of
 ;; the form
